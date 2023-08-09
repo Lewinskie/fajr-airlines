2e95189d9d289f0557eb3dff10af348664b7b0a2
Lewinskie <wangaralewis@gmail.com>
2023-08-09 08:18:22 +0300

    Added Who are we title and a little summary about the company in the about us page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,12 @@
 import CeoMessage from "@/components/templates/Sections/ceoMessage";
 import AboutSplash from "@/components/templates/Sections/aboutSplash";
-import { team } from "../data";
+import { team, about_us } from "../data";
 
 function About({}) {
   const teamlist = team.staff;
   return (
     <>
-      <AboutSplash teamlist={teamlist} />
+      <AboutSplash teamlist={teamlist} about_us={about_us} />
     </>
   );
 }
