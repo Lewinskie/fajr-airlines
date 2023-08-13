f1a6ccca55360af3261fb079b3243dd6bb61d887
Lewinskie <wangaralewis@gmail.com>
2023-08-13 11:38:15 +0300

    cleanups

--- src/components/molecules/navLinkList.jsx
+++ src/components/molecules/navLinkList.jsx
@@ -13,7 +13,6 @@ function NavLinkList({ sticky }) {
           NavTitles.map((NavTitle) => {
             return (
               <div key={NavTitle.id} className="menu-item">
-<<<<<<< HEAD
                 <Link
                   href={NavTitle.href}
                   className={`${
@@ -38,35 +37,6 @@ function NavLinkList({ sticky }) {
                       ))}
                   </ul>
                 )}
-=======
-              <Link
-                href={NavTitle.href}
-                className={`${
-                  router.asPath == NavTitle.href
-                    ? `${
-                        sticky
-                          ? "border-b-4 border-secondary-500 text-primary"
-                          : "text-primary-500 border-b-4 border-secondary-500"
-                      }`
-                    : sticky
-                    ? "text-gray-500"
-                    : "text-white-500"
-                } text-base font-sans hover:border-b-4 hover:border-secondary-500 hover:text-primary-500 `}
-              >
-                {NavTitle.name}
-              </Link>
-              <ul className="dropdown-menu">
-                {console.log({subMenu: NavTitle.sub_menu})}
-                {NavTitle?.sub_menu && NavTitle.sub_menu.length > 0 && NavTitle.sub_menu.map(item => (
-                  <li key={item.id}>
-                    <Link href="/" >
-                      {item.sub_name}
-                    </Link>
-                </li>
-                ))}
-                
-              </ul>
->>>>>>> 5d0b71a163bdf87111ed0823a9672b37bbd9ffb2
               </div>
             );
           })}
